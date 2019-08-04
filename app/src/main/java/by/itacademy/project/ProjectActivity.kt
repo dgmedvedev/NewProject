4975bdcab4da4c8cef5e362aba99bda8f5439cd9
Администратор <dg-medvedev@mail.ru>
2019-08-04 12:14:55 +0300

    commit

--- app/src/main/java/by/itacademy/project/ProjectActivity.kt
+++ app/src/main/java/by/itacademy/project/ProjectActivity.kt
@@ -7,5 +7,6 @@ class ProjectActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
+                //  дордлгр
     }
 }
